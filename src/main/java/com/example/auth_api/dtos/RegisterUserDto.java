753301ab23282b8b04d81504604f14ba1d31e29e
Michal <michal.golawski.mg@gmail.com>
2024-12-14 18:07:24 +0100

    Added login and register dtos

---
+++ src/main/java/com/example/auth_api/dtos/RegisterUserDto.java
@@ -0,0 +1,40 @@
+package com.example.auth_api.dtos;
+
+public class RegisterUserDto {
+    private Integer id;
+    private String email;
+    private String role;
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
